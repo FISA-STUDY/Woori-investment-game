a09e090aadb0237cf93044a159876fb5c09d3b7e
unknown <ricky0130@naver.com>
2025-07-04 18:16:42 +0900

    feat:뉴스 랜덤추출

--- src/controller/NewsGenerator.java
+++ src/controller/NewsGenerator.java
@@ -1,6 +1,14 @@
 package controller;
 
+import model.Model;
+import model.domain.News;
 
 public class NewsGenerator {
+	private static Model model = Model.getModel();
 	
+	News[] news = model.getNewses();
+
+	public News generateNews() {
+		return news[(int)(Math.random()*9)];
+	}
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,14 +1,18 @@
 package model;
 
+import model.domain.News;
+
 public class Model {
 	private Database db = new Database();
 	
 	private static Model model = new Model();
 	
-	// Singleton design pattern 구조
 	public static Model getModel() {
 		return model;
 	}
 	private Model() {}
 	
+	public News[] getNewses() {
+		return db.getNews();
+	}
 }
