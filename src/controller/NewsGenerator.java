2ca17c9b41e4aafb95524b924bc45c7aa517d216
ChatHongPT <suk924600@naver.com>
2025-07-07 12:34:45 +0900

    feat: news 기능 구현

--- src/controller/NewsGenerator.java
+++ src/controller/NewsGenerator.java
@@ -5,24 +5,16 @@ import model.domain.News;
 
 public class NewsGenerator {
     
-    /**
-     * 랜덤한 뉴스 생성
-     */
     public News generateNews() {
         return Database.getRandomNews();
     }
     
-    /**
-     * 특정 회사의 랜덤 뉴스 생성
-     */
+
     public News generateNewsForCompany(String companyName) {
         return Database.getRandomNewsByCompany(companyName);
     }
     
-    /**
-     * 뉴스 생성 후 주식 가격에 바로 반영
-     * 이 메서드가 핵심입니다!
-     */
+ 
     public News generateNewsAndApplyPriceChange() {
         // 1. 랜덤 뉴스 생성
         News todayNews = generateNews();
@@ -33,9 +25,6 @@ public class NewsGenerator {
         return todayNews;
     }
     
-    /**
-     * 여러 개의 뉴스 생성 (하루에 여러 뉴스가 나올 때)
-     */
     public News[] generateMultipleNews(int count) {
         News[] dailyNews = new News[count];
         
