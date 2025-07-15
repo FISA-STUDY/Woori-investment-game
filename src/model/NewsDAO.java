5fd1e54d8f25ea641bd1535bc90124ed3139d053
ottffss1005 <ottffss1005@naver.com>
2025-07-15 15:56:38 +0900

    asd

--- src/model/NewsDAO.java
+++ src/model/NewsDAO.java
@@ -9,7 +9,7 @@ import util.DBUtil;
 
 public class NewsDAO {
 
-	private static NewsDAO newsDAO = new NewsDAO();
+   private static NewsDAO newsDAO = new NewsDAO();
 
     public static NewsDAO getNewsDAO() {
         return newsDAO;
@@ -18,37 +18,29 @@ public class NewsDAO {
     private NewsDAO() {}
     
     public News getNews() throws Exception{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		News news = null;
-		try {
-			conn = DBUtil.getConnection();
-			stmt=conn.createStatement();
-			rs=stmt.executeQuery("select * from News ORDER BY RAND() limit 1");
-			
-			
-<<<<<<< HEAD
-			news = new News(rs.getInt("id"),
-			rs.getBoolean("n_isGood"),
-			rs.getString("n_message"));
-=======
-			news = new News(); // 10개의 메모리 증가
-			new News(rs.getInt("empno"),
-			rs.getString("ename"),
-			rs.getInt("deptno"));
-			}
->>>>>>> 9c54e19 (feat: 로그인/회원가입 기능 구현)
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			
-			DBUtil.close(conn,stmt,rs);
-			
-		}
-		return news;
-	}
+      Connection conn = null;
+      Statement stmt = null;
+      ResultSet rs = null;
+      
+      News news = null;
+      try {
+         conn = DBUtil.getConnection();
+         stmt=conn.createStatement();
+         rs=stmt.executeQuery("select * from News ORDER BY RAND() limit 1");
+         
+         
+         news = new News(rs.getInt("id"),
+         rs.getBoolean("n_isGood"),
+         rs.getString("n_message"));
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         
+         DBUtil.close(conn,stmt,rs);
+         
+      }
+      return news;
+   }
     
-	
+   
 }
